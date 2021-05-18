c5be08113d070f79a986b467cebb3ee217ede86d
ErmakovEA <70837033+ErmakovEA@users.noreply.github.com>
2021-05-18 14:17:21 +0300

    Update StudentClass.h

--- StudentClass.h
+++ StudentClass.h
@@ -16,10 +16,10 @@ struct StudentNode
 	string faculty;
 	string department;
 	string group;
-	string recordСardNumber;
+	string recordРЎardNumber;
 	string birthDateString;
-	bool sex; // true - мальчик
-			  // false - девочка
+	bool sex; // true - РјР°Р»СЊС‡РёРє
+			  // false - РґРµРІРѕС‡РєР°
 	int startYear;
 
 	ExamsRecords examsRecordsData[9][10];
@@ -44,23 +44,23 @@ public:
 
 	void addRusakov() {
 		sn->id = 0;
-		sn->surName = "Русаков";
-		sn->name = "Алексей";
-		sn->middleName = "Михайлович";
-		sn->faculty = "10.05.04 Информационно-аналитические системы безопасности";
-		sn->department = "Институт комплексной безопасности и специального приборостроения";
-		sn->group = "БИСО-01-21";
-		sn->recordСardNumber = "20Б0857";
+		sn->surName = "Р СѓСЃР°РєРѕРІ";
+		sn->name = "РђР»РµРєСЃРµР№";
+		sn->middleName = "РњРёС…Р°Р№Р»РѕРІРёС‡";
+		sn->faculty = "10.05.04 РРЅС„РѕСЂРјР°С†РёРѕРЅРЅРѕ-Р°РЅР°Р»РёС‚РёС‡РµСЃРєРёРµ СЃРёСЃС‚РµРјС‹ Р±РµР·РѕРїР°СЃРЅРѕСЃС‚Рё";
+		sn->department = "РРЅСЃС‚РёС‚СѓС‚ РєРѕРјРїР»РµРєСЃРЅРѕР№ Р±РµР·РѕРїР°СЃРЅРѕСЃС‚Рё Рё СЃРїРµС†РёР°Р»СЊРЅРѕРіРѕ РїСЂРёР±РѕСЂРѕСЃС‚СЂРѕРµРЅРёСЏ";
+		sn->group = "Р‘РРЎРћ-01-21";
+		sn->recordРЎardNumber = "20Р‘0857";
 		sn->sex = true;
 		sn->startYear = 2021;
 		sn->birthDateString = "27.12.1984";
 		ExamsResultsClass* er = new ExamsResultsClass();
-		er->add(0, "Яыки программирования 1", 5);
-		er->add(0, "Математика 1", 5);
-		er->add(0, "Физкультура 1", true);
-		er->add(1, "Яыки программирования 2", 5);
-		er->add(1, "Математика 2", 5);
-		er->add(1, "Физкультура 2", true);
+		er->add(0, "РЇС‹РєРё РїСЂРѕРіСЂР°РјРјРёСЂРѕРІР°РЅРёСЏ 1", 5);
+		er->add(0, "РњР°С‚РµРјР°С‚РёРєР° 1", 5);
+		er->add(0, "Р¤РёР·РєСѓР»СЊС‚СѓСЂР° 1", true);
+		er->add(1, "РЇС‹РєРё РїСЂРѕРіСЂР°РјРјРёСЂРѕРІР°РЅРёСЏ 2", 5);
+		er->add(1, "РњР°С‚РµРјР°С‚РёРєР° 2", 5);
+		er->add(1, "Р¤РёР·РєСѓР»СЊС‚СѓСЂР° 2", true);
 		setExamsResultsData(er, sn);
 	}
 	void setExamsResultsData(ExamsResultsClass* er, StudentNode* sn) {
@@ -80,7 +80,7 @@ public:
 		stringMas.Add(sb.setParam("faculty", sn->faculty));
 		stringMas.Add(sb.setParam("department", sn->department));
 		stringMas.Add(sb.setParam("group", sn->group));
-		stringMas.Add(sb.setParam("recordСardNumber", sn->recordСardNumber));
+		stringMas.Add(sb.setParam("recordРЎardNumber", sn->recordРЎardNumber));
 		int intSex = 1;
 		if (sn->sex)
 			intSex = 1;
@@ -94,9 +94,9 @@ public:
 		for (int sem = 0; sem < 9; sem++)
 			for (int num = 0; num < 10; num++)
 				if (not sn->examsRecordsData[sem][num].isEmpty) {
-					//examsResults_sem_num_n - название экзамена или зачета
+					//examsResults_sem_num_n - РЅР°Р·РІР°РЅРёРµ СЌРєР·Р°РјРµРЅР° РёР»Рё Р·Р°С‡РµС‚Р°
 					stringMas.Add(sb.setParam("examsResults_" + std::to_string(sem) + "_" + std::to_string(num) + "_n", sn->examsRecordsData[sem][num].name));
-					//examsResults_sem_num_m - оценка
+					//examsResults_sem_num_m - РѕС†РµРЅРєР°
 					stringMas.Add(sb.setParam("examsResults_" + std::to_string(sem) + "_" + std::to_string(num) + "_m", std::to_string(sn->examsRecordsData[sem][num].mark)));
 				}
 		//stringMas.dislay();
@@ -111,9 +111,9 @@ public:
 		ClassMenu* sexMenu = new ClassMenu();
 		int resultSelectedItem = 1;
 		const int exitItem = 3;
-		sexMenu->addTitleItem("Выберите пол: ");
-		sexMenu->addItem("Мальчик:(");
-		sexMenu->addItem("Девочка:(");
+		sexMenu->addTitleItem("Р’С‹Р±РµСЂРёС‚Рµ РїРѕР»: ");
+		sexMenu->addItem("РњР°Р»СЊС‡РёРє:(");
+		sexMenu->addItem("Р”РµРІРѕС‡РєР°:(");
 		while (resultSelectedItem != exitItem) {
 			sexMenu->run();
 			resultSelectedItem = sexMenu->getSelectedItem();
@@ -137,30 +137,30 @@ public:
 		int resultStudDataMenu = 1;
 		const int exitStudDataMenu = 0;
 		ClassEdit* ce = new  ClassEdit();
-		studDataMenu->addItem("Выход");   //0
-		studDataMenu->addItem("Добавить/изменить фамилию"); //1
-		studDataMenu->addItem("Добавить/изменить имя");   //2
-		studDataMenu->addItem("Добавить/изменить отчество");   //3
-		studDataMenu->addItem("Добавить/изменить институт");   //4
-		studDataMenu->addItem("Добавить/изменить кафедру");   //5
-		studDataMenu->addItem("Добавить/изменить группу");   //6
-		studDataMenu->addItem("Добавить/изменить номер зачетной книжки");   //7
-		studDataMenu->addItem("Добавить/изменить пол");   //
-		studDataMenu->addItem("Добавить/изменить год поступления в ВУЗ");   //8
-		studDataMenu->addItem("Добавить/изменить дату рождения");   //9
-		studDataMenu->addItem("Добавить/изменить оценки");   //10
+		studDataMenu->addItem("Р’С‹С…РѕРґ");   //0
+		studDataMenu->addItem("Р”РѕР±Р°РІРёС‚СЊ/РёР·РјРµРЅРёС‚СЊ С„Р°РјРёР»РёСЋ"); //1
+		studDataMenu->addItem("Р”РѕР±Р°РІРёС‚СЊ/РёР·РјРµРЅРёС‚СЊ РёРјСЏ");   //2
+		studDataMenu->addItem("Р”РѕР±Р°РІРёС‚СЊ/РёР·РјРµРЅРёС‚СЊ РѕС‚С‡РµСЃС‚РІРѕ");   //3
+		studDataMenu->addItem("Р”РѕР±Р°РІРёС‚СЊ/РёР·РјРµРЅРёС‚СЊ РёРЅСЃС‚РёС‚СѓС‚");   //4
+		studDataMenu->addItem("Р”РѕР±Р°РІРёС‚СЊ/РёР·РјРµРЅРёС‚СЊ РєР°С„РµРґСЂСѓ");   //5
+		studDataMenu->addItem("Р”РѕР±Р°РІРёС‚СЊ/РёР·РјРµРЅРёС‚СЊ РіСЂСѓРїРїСѓ");   //6
+		studDataMenu->addItem("Р”РѕР±Р°РІРёС‚СЊ/РёР·РјРµРЅРёС‚СЊ РЅРѕРјРµСЂ Р·Р°С‡РµС‚РЅРѕР№ РєРЅРёР¶РєРё");   //7
+		studDataMenu->addItem("Р”РѕР±Р°РІРёС‚СЊ/РёР·РјРµРЅРёС‚СЊ РїРѕР»");   //
+		studDataMenu->addItem("Р”РѕР±Р°РІРёС‚СЊ/РёР·РјРµРЅРёС‚СЊ РіРѕРґ РїРѕСЃС‚СѓРїР»РµРЅРёСЏ РІ Р’РЈР—");   //8
+		studDataMenu->addItem("Р”РѕР±Р°РІРёС‚СЊ/РёР·РјРµРЅРёС‚СЊ РґР°С‚Сѓ СЂРѕР¶РґРµРЅРёСЏ");   //9
+		studDataMenu->addItem("Р”РѕР±Р°РІРёС‚СЊ/РёР·РјРµРЅРёС‚СЊ РѕС†РµРЅРєРё");   //10
 		int day, month, year;
 		while (resultStudDataMenu != exitStudDataMenu) {
 			studDataMenu->eraseTitle();
-			studDataMenu->addTitleItem("Изменение/добавление данных о студенте:");
-			studDataMenu->addTitleItem("Фамилия: " + sn->surName + " Имя: " + sn->name + " Отчество: " + sn->middleName);
+			studDataMenu->addTitleItem("РР·РјРµРЅРµРЅРёРµ/РґРѕР±Р°РІР»РµРЅРёРµ РґР°РЅРЅС‹С… Рѕ СЃС‚СѓРґРµРЅС‚Рµ:");
+			studDataMenu->addTitleItem("Р¤Р°РјРёР»РёСЏ: " + sn->surName + " РРјСЏ: " + sn->name + " РћС‚С‡РµСЃС‚РІРѕ: " + sn->middleName);
 			string sexString = "";
-			if (sn->sex) { sexString = "мальчик:("; }
-			else { sexString = "девочка;)"; }
-			studDataMenu->addTitleItem("пол: " + sexString + " дата рождения: " + sn->birthDateString + " год поступления:" + std::to_string(sn->startYear));
-			studDataMenu->addTitleItem("Номер зачетной книжки: " + sn->recordСardNumber + " Группа: " + sn->group);
-			studDataMenu->addTitleItem("Институт: " + sn->faculty);
-			studDataMenu->addTitleItem("Кафедра: " + sn->department);
+			if (sn->sex) { sexString = "РјР°Р»СЊС‡РёРє:("; }
+			else { sexString = "РґРµРІРѕС‡РєР°;)"; }
+			studDataMenu->addTitleItem("РїРѕР»: " + sexString + " РґР°С‚Р° СЂРѕР¶РґРµРЅРёСЏ: " + sn->birthDateString + " РіРѕРґ РїРѕСЃС‚СѓРїР»РµРЅРёСЏ:" + std::to_string(sn->startYear));
+			studDataMenu->addTitleItem("РќРѕРјРµСЂ Р·Р°С‡РµС‚РЅРѕР№ РєРЅРёР¶РєРё: " + sn->recordРЎardNumber + " Р“СЂСѓРїРїР°: " + sn->group);
+			studDataMenu->addTitleItem("РРЅСЃС‚РёС‚СѓС‚: " + sn->faculty);
+			studDataMenu->addTitleItem("РљР°С„РµРґСЂР°: " + sn->department);
 			studDataMenu->run();
 			resultStudDataMenu = studDataMenu->getSelectedItem();
 			string tmpString = "";
@@ -173,39 +173,39 @@ public:
 				resultStudDataMenu = exitStudDataMenu;
 				break;
 			case 1:
-				ce->setLabel("Введите фамилию. ");
+				ce->setLabel("Р’РІРµРґРёС‚Рµ С„Р°РјРёР»РёСЋ. ");
 				sn->surName = ce->setDataString(sn->surName);
 				break;
 			case 2:
-				ce->setLabel("Введите имя. ");
+				ce->setLabel("Р’РІРµРґРёС‚Рµ РёРјСЏ. ");
 				sn->name = ce->setDataString(sn->name);
 				break;
 			case 3:
-				ce->setLabel("Введите отчество. ");
+				ce->setLabel("Р’РІРµРґРёС‚Рµ РѕС‚С‡РµСЃС‚РІРѕ. ");
 				sn->middleName = ce->setDataString(sn->middleName);
 				break;
 			case 4:
-				ce->setLabel("Введите название института. ");
+				ce->setLabel("Р’РІРµРґРёС‚Рµ РЅР°Р·РІР°РЅРёРµ РёРЅСЃС‚РёС‚СѓС‚Р°. ");
 				sn->faculty = ce->setDataString(sn->faculty);
 				break;
 			case 5:
-				ce->setLabel("Введите название кафедры. ");
+				ce->setLabel("Р’РІРµРґРёС‚Рµ РЅР°Р·РІР°РЅРёРµ РєР°С„РµРґСЂС‹. ");
 				sn->department = ce->setDataString(sn->department);
 				break;
 			case 6:
-				ce->setLabel("Введите группу. ");
+				ce->setLabel("Р’РІРµРґРёС‚Рµ РіСЂСѓРїРїСѓ. ");
 				sn->group = ce->setDataString(sn->group);
 				break;
 			case 7:
-				ce->setLabel("Введите номер зачетной книжки. ");
-				sn->recordСardNumber = ce->setDataString(sn->recordСardNumber);
+				ce->setLabel("Р’РІРµРґРёС‚Рµ РЅРѕРјРµСЂ Р·Р°С‡РµС‚РЅРѕР№ РєРЅРёР¶РєРё. ");
+				sn->recordРЎardNumber = ce->setDataString(sn->recordРЎardNumber);
 				break;
 			case 8:
-				ce->setLabel("Введите пол. ");
+				ce->setLabel("Р’РІРµРґРёС‚Рµ РїРѕР». ");
 				sn->sex = editSex();        ////
 				break;
 			case 9:
-				ce->setLabel("Введите год поступления в ВУЗ. ");
+				ce->setLabel("Р’РІРµРґРёС‚Рµ РіРѕРґ РїРѕСЃС‚СѓРїР»РµРЅРёСЏ РІ Р’РЈР—. ");
 				startYear = ce->setDataInt(1900, 2021);
 				tmpString = sb->split(sn->birthDateString.c_str(), '.', 3);
 				year = atoi(tmpString.c_str());
@@ -218,18 +218,18 @@ public:
 					}
 					else
 					{
-						cout << "Ошибка год поступления в институт должен быть на 15 лет больше чем год рождения";
+						cout << "РћС€РёР±РєР° РіРѕРґ РїРѕСЃС‚СѓРїР»РµРЅРёСЏ РІ РёРЅСЃС‚РёС‚СѓС‚ РґРѕР»Р¶РµРЅ Р±С‹С‚СЊ РЅР° 15 Р»РµС‚ Р±РѕР»СЊС€Рµ С‡РµРј РіРѕРґ СЂРѕР¶РґРµРЅРёСЏ";
 						_getch();
 					}
 				}
 
 				break;
 			case 10:
-				ce->setLabel("Введите день рождения. ");
+				ce->setLabel("Р’РІРµРґРёС‚Рµ РґРµРЅСЊ СЂРѕР¶РґРµРЅРёСЏ. ");
 				day = ce->setDataInt(1, 31);
-				ce->setLabel("Введите месяц рождения. ");
+				ce->setLabel("Р’РІРµРґРёС‚Рµ РјРµСЃСЏС† СЂРѕР¶РґРµРЅРёСЏ. ");
 				month = ce->setDataInt(1, 12);
-				ce->setLabel("Введите год рождения. ");
+				ce->setLabel("Р’РІРµРґРёС‚Рµ РіРѕРґ СЂРѕР¶РґРµРЅРёСЏ. ");
 				year = ce->setDataInt(1900, 2005);
 				if (sn->startYear == 0)
 					sn->birthDateString = std::to_string(day) + "." + std::to_string(month) + "." + std::to_string(year);
@@ -240,13 +240,13 @@ public:
 					}
 					else
 					{
-						cout << "Ошибка год поступления в институт должен быть на 15 лет больше чем год рождения";
+						cout << "РћС€РёР±РєР° РіРѕРґ РїРѕСЃС‚СѓРїР»РµРЅРёСЏ РІ РёРЅСЃС‚РёС‚СѓС‚ РґРѕР»Р¶РµРЅ Р±С‹С‚СЊ РЅР° 15 Р»РµС‚ Р±РѕР»СЊС€Рµ С‡РµРј РіРѕРґ СЂРѕР¶РґРµРЅРёСЏ";
 						_getch();
 					}
 				}
 				break;
 			case 11:
-				ce->setLabel("Просмотреть/ изменить оценки.");
+				ce->setLabel("РџСЂРѕСЃРјРѕС‚СЂРµС‚СЊ/ РёР·РјРµРЅРёС‚СЊ РѕС†РµРЅРєРё.");
 				//sn = getStudentNode();
 				for (int i = 0; i < 9; i++)
 					for (int j = 0; j < 10; j++) {
@@ -255,9 +255,9 @@ public:
 						oldExamsRecordsData[i][j].mark = sn->examsRecordsData[i][j].mark;
 					}
 
-				editExamsResults(sn); // "Ошибка есть одинаковые названия предметов"
+				editExamsResults(sn); // "РћС€РёР±РєР° РµСЃС‚СЊ РѕРґРёРЅР°РєРѕРІС‹Рµ РЅР°Р·РІР°РЅРёСЏ РїСЂРµРґРјРµС‚РѕРІ"
 				if (isSameNameExamsResults(sn)) {
-					cout << "Ошибка есть одинаковые названия предметов";
+					cout << "РћС€РёР±РєР° РµСЃС‚СЊ РѕРґРёРЅР°РєРѕРІС‹Рµ РЅР°Р·РІР°РЅРёСЏ РїСЂРµРґРјРµС‚РѕРІ";
 					for (int i = 0; i < 9; i++)
 						for (int j = 0; j < 10; j++) {
 							sn->examsRecordsData[i][j].name = oldExamsRecordsData[i][j].name;
@@ -282,14 +282,14 @@ public:
 		const int exitS = 0;
 		ClassEdit* ce = new  ClassEdit();
 		ExamsResultsClass* erc = new ExamsResultsClass();
-		sesMenu->addItem("Выход");   //0
+		sesMenu->addItem("Р’С‹С…РѕРґ");   //0
 		for (int i = 1; i < 10; i++) {
-			sesMenu->addItem("Сессия " + std::to_string(i));
+			sesMenu->addItem("РЎРµСЃСЃРёСЏ " + std::to_string(i));
 		}
 		while (result != exit) {
 			sesMenu->eraseTitle();
-			sesMenu->addTitleItem("Просмотр/изменение/добавление данных о оценках за все сессии");
-			sesMenu->addTitleItem("Выберите сессию для просмотра и редактирования информации об оценках");
+			sesMenu->addTitleItem("РџСЂРѕСЃРјРѕС‚СЂ/РёР·РјРµРЅРµРЅРёРµ/РґРѕР±Р°РІР»РµРЅРёРµ РґР°РЅРЅС‹С… Рѕ РѕС†РµРЅРєР°С… Р·Р° РІСЃРµ СЃРµСЃСЃРёРё");
+			sesMenu->addTitleItem("Р’С‹Р±РµСЂРёС‚Рµ СЃРµСЃСЃРёСЋ РґР»СЏ РїСЂРѕСЃРјРѕС‚СЂР° Рё СЂРµРґР°РєС‚РёСЂРѕРІР°РЅРёСЏ РёРЅС„РѕСЂРјР°С†РёРё РѕР± РѕС†РµРЅРєР°С…");
 			sesMenu->run();
 			result = sesMenu->getSelectedItem();
 			if (result == exit) {
@@ -299,27 +299,27 @@ public:
 			int curSess = result;
 			if ((curSess >= 1) and (curSess <= 10))
 			{
-				//рисуем меню и правим оценки про сессию
+				//СЂРёСЃСѓРµРј РјРµРЅСЋ Рё РїСЂР°РІРёРј РѕС†РµРЅРєРё РїСЂРѕ СЃРµСЃСЃРёСЋ
 				msMenu->eraseAll();
-				msMenu->addTitleItem("Просмотр/изменение/добавление данных о оценках");
-				msMenu->addTitleItem("Данные сессии №" + to_string(curSess));
+				msMenu->addTitleItem("РџСЂРѕСЃРјРѕС‚СЂ/РёР·РјРµРЅРµРЅРёРµ/РґРѕР±Р°РІР»РµРЅРёРµ РґР°РЅРЅС‹С… Рѕ РѕС†РµРЅРєР°С…");
+				msMenu->addTitleItem("Р”Р°РЅРЅС‹Рµ СЃРµСЃСЃРёРё в„–" + to_string(curSess));
 				resultS = 1;
 				while (resultS != exitS) {
 					msMenu->eraseItem();
-					msMenu->addItem("Выход");
-					msMenu->addItem("Добавить запись");
-					msMenu->addItem("Удалить запись");
+					msMenu->addItem("Р’С‹С…РѕРґ");
+					msMenu->addItem("Р”РѕР±Р°РІРёС‚СЊ Р·Р°РїРёСЃСЊ");
+					msMenu->addItem("РЈРґР°Р»РёС‚СЊ Р·Р°РїРёСЃСЊ");
 					for (int i = 0; i < 10; i++)
 						if (not sn->examsRecordsData[curSess - 1][i].isEmpty)
 						{
 							string markString = "";
 							int markInt = sn->examsRecordsData[curSess - 1][i].mark;
-							if (markInt == 0) markString = "не зачтено";
-							if (markInt == 1) markString = "зачтено";
+							if (markInt == 0) markString = "РЅРµ Р·Р°С‡С‚РµРЅРѕ";
+							if (markInt == 1) markString = "Р·Р°С‡С‚РµРЅРѕ";
 							if ((markInt >= 2) and (markInt <= 5)) {
 								markString = to_string(markInt);
 							}
-							msMenu->addItem("Предмет: " + sn->examsRecordsData[curSess - 1][i].name + " Оценка: " + markString);
+							msMenu->addItem("РџСЂРµРґРјРµС‚: " + sn->examsRecordsData[curSess - 1][i].name + " РћС†РµРЅРєР°: " + markString);
 						}
 					msMenu->run();
 					resultS = msMenu->getSelectedItem();
@@ -329,7 +329,7 @@ public:
 						int itemNum = msMenu->getItemsCount() - 3;
 						if (itemNum > 9) {
 							system("cls");
-							cout << "Ошибка в сессию бывает только 10 дисциплин для зачетов или экзаменов";
+							cout << "РћС€РёР±РєР° РІ СЃРµСЃСЃРёСЋ Р±С‹РІР°РµС‚ С‚РѕР»СЊРєРѕ 10 РґРёСЃС†РёРїР»РёРЅ РґР»СЏ Р·Р°С‡РµС‚РѕРІ РёР»Рё СЌРєР·Р°РјРµРЅРѕРІ";
 							_getch();  //!!!!!!!!!!!!!!!????????
 							_getch();
 						}
@@ -338,7 +338,7 @@ public:
 						}
 					}
 					if (resultS == 2) {
-						//Удалить запись
+						//РЈРґР°Р»РёС‚СЊ Р·Р°РїРёСЃСЊ
 						delExamsResults(sn, curSess);
 					}
 					if (resultS > 2) {
@@ -351,14 +351,14 @@ public:
 	}
 	void addExamsResults(StudentNode* sn, int curSess, int curItem) {
 		ClassEdit* ce = new  ClassEdit();
-		ce->setLabel("Введите название предмета. ");
+		ce->setLabel("Р’РІРµРґРёС‚Рµ РЅР°Р·РІР°РЅРёРµ РїСЂРµРґРјРµС‚Р°. ");
 		sn->examsRecordsData[curSess - 1][curItem].name = ce->setDataString(sn->examsRecordsData[curSess - 1][curItem].name);
 		int resultS = 1;
 		const int exitS = 0;
 		ClassMenu* msMenu = new ClassMenu();
-		msMenu->addTitleItem("Выберите оценку");
-		msMenu->addItem("не зачтено"); //0
-		msMenu->addItem("зачтено"); //1
+		msMenu->addTitleItem("Р’С‹Р±РµСЂРёС‚Рµ РѕС†РµРЅРєСѓ");
+		msMenu->addItem("РЅРµ Р·Р°С‡С‚РµРЅРѕ"); //0
+		msMenu->addItem("Р·Р°С‡С‚РµРЅРѕ"); //1
 		msMenu->addItem("2"); //2	
 		msMenu->addItem("3"); //3		
 		msMenu->addItem("4"); //4		
@@ -367,9 +367,9 @@ public:
 		while (resultS != exitS) {
 			msMenu->run();
 			resultS = msMenu->getSelectedItem();
-			// 0 - не зачет  
-			// 1 - зачет  
-			// 2,3,4,5 - оценки
+			// 0 - РЅРµ Р·Р°С‡РµС‚  
+			// 1 - Р·Р°С‡РµС‚  
+			// 2,3,4,5 - РѕС†РµРЅРєРё
 			sn->examsRecordsData[curSess - 1][curItem].mark = resultS;
 			resultS = exitS;
 		}
@@ -380,22 +380,22 @@ public:
 		int resultS = 1;
 		const int exitS = 0;
 		ClassMenu* msMenu = new ClassMenu();
-		msMenu->addTitleItem("Выберите запись для удаления");
+		msMenu->addTitleItem("Р’С‹Р±РµСЂРёС‚Рµ Р·Р°РїРёСЃСЊ РґР»СЏ СѓРґР°Р»РµРЅРёСЏ");
 		resultS = 1;
 		while (resultS != exitS) {
 			msMenu->eraseItem();
-			msMenu->addItem("Выход");
+			msMenu->addItem("Р’С‹С…РѕРґ");
 			for (int i = 0; i < 10; i++)
 				if (not sn->examsRecordsData[curSess - 1][i].isEmpty)
 				{
 					string markString = "";
 					int markInt = sn->examsRecordsData[curSess - 1][i].mark;
-					if (markInt == 0) markString = "не зачтено";
-					if (markInt == 1) markString = "зачтено";
+					if (markInt == 0) markString = "РЅРµ Р·Р°С‡С‚РµРЅРѕ";
+					if (markInt == 1) markString = "Р·Р°С‡С‚РµРЅРѕ";
 					if ((markInt >= 2) and (markInt <= 5)) {
 						markString = std::to_string(markInt);
 					}
-					msMenu->addItem("Предмет: " + sn->examsRecordsData[curSess - 1][i].name + " Оценка: " + markString);
+					msMenu->addItem("РџСЂРµРґРјРµС‚: " + sn->examsRecordsData[curSess - 1][i].name + " РћС†РµРЅРєР°: " + markString);
 				}
 			msMenu->run();
 			resultS = msMenu->getSelectedItem();
@@ -404,12 +404,12 @@ public:
 			else
 			{
 				sn->examsRecordsData[curSess - 1][resultS - 1].isEmpty = true;
-				//Дефрагментация массива
+				//Р”РµС„СЂР°РіРјРµРЅС‚Р°С†РёСЏ РјР°СЃСЃРёРІР°
 				ExamsResultsClass* er = new ExamsResultsClass();
 				for (int i = 0; i < 10; i++)
 					if (not sn->examsRecordsData[curSess - 1][i].isEmpty)
 						er->add(0, sn->examsRecordsData[curSess - 1][i].name, sn->examsRecordsData[curSess - 1][i].mark);
-				//er->add(0, "Яыки программирования 1", 5);
+				//er->add(0, "РЇС‹РєРё РїСЂРѕРіСЂР°РјРјРёСЂРѕРІР°РЅРёСЏ 1", 5);
 				for (int i = 0; i < 10; i++)
 					sn->examsRecordsData[curSess - 1][i].isEmpty = true;
 				for (int i = 0; i < 10; i++)
@@ -450,7 +450,7 @@ public:
 	double getAvrMarks1(StudentNode* sn) {
 		double sum = 0;
 		double count = 0;
-		for (int i = 1; i < 2; i++) {
+		for (int i = 0; i < 1; i++) {
 			for (int j = 0; j < 10; j++)
 				if (sn->examsRecordsData[i][j].isEmpty == false) {
 					sum = sum + sn->examsRecordsData[i][j].mark;
